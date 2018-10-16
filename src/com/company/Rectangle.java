a3e5296e2d23c99b3d172174321b91a553187634
rcooper3307 <rcooper3307@bths.edu>
2018-10-16 08:42:43 -0400

    ethj

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Rectangle extends Shape {
+    private int side;
+    public Rectangle( int side)
+    {
+        super("Rectangle");
+        this.side = side;
+    }
+    @Override
+    public double getArea
+}
